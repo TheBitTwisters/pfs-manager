c0b20c9a1d5fe0bf864b8112ffe716bcde3ce9c1
Michael Angelo Sison <michael01angelo@gmail.com>
2018-12-19 17:00:10 +0800

    Logs update

--- logs/models.py
+++ logs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Log(models.Model):
+    object = models.CharField(max_length=50, blank=False)
+    object_id = models.IntegerField(default=0)
+    action = models.CharField(max_length=50, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    datetime = models.DateTimeField(default=timezone.now)
--- logs/views.py
+++ logs/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Log
+import datetime
 
-# Create your views here.
+
+@login_required
+def index(request):
+    logs = Log.objects.all().order_by('-id')[:100]
+    data = { 'logs': logs }
+    return render(request, 'logs/index.html', data)
