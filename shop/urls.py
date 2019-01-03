5d6b8dd2fd5aa19f7ab4c7d0e4ee3b0d9df6c21e
Michael Angelo Sison <michael01angelo@gmail.com>
2019-01-03 16:52:50 +0800

    Quote updates

--- shop/urls.py
+++ shop/urls.py
@@ -6,5 +6,7 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.quotes, name='index'),
     path('quotes/', views.quotes, name='quotes'),
-    path('quote_edit/<int:quote_id>/', views.quotes, name='quote_edit'),
+    path('quote_edit/<int:quote_id>/', views.quote_edit, name='quote_edit'),
+    path('quote_save', views.quote_save, name='quote_save'),
+    path('quote_delete', views.quote_delete, name='quote_delete'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,9 +2,72 @@ from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist
-import datetime
+from django.utils import timezone
+from .models import Quote
+from logs.models import Log
 
 
 @login_required
 def quotes(request):
-    return render(request, 'shop/quotes.html')
+    quotes = Quote.objects.filter(deleted=False).order_by('-id')[:30]
+    data = { 'quotes': quotes }
+    return render(request, 'shop/quotes.html', data)
+
+
+@login_required
+def quote_edit(request, quote_id=0):
+    quote = None
+    try:
+        quote = Quote.objects.get(id=quote_id)
+    except ObjectDoesNotExist:
+        quote = Quote()
+        quote.id = 0
+    data = { 'quote': quote }
+    return render(request, 'shop/quote_edit.html', data)
+
+
+@login_required
+def quote_save(request):
+    quote_id = request.POST.get('quote_id', 0)
+    action = 'create'
+    if request.method == 'POST':
+        quote = Quote()
+        try:
+            if int(quote_id) > 0:
+                quote.id = quote_id
+                action = 'update'
+                quote.datetime_updated = timezone.now()
+        except ValueError as e:
+            quote.id = 0
+        quote.form(request.POST)
+        quote.user = request.user
+        if quote.is_valid():
+            quote.save()
+            Log.objects.create(
+                object = 'Quote',
+                object_id = quote.id,
+                action = action,
+                user = request.user
+            )
+            return redirect('shop:quotes')
+    return redirect('shop:quote_edit', quote_id)
+
+
+@login_required
+def quote_delete(request):
+    quote_id = request.POST.get('quote_id', 0)
+    if request.method == 'POST':
+        try:
+            quote = Quote.objects.get(id=quote_id)
+            quote.deleted = True
+            quote.datetime_deleted = timezone.now()
+            quote.save()
+            Log.objects.create(
+                object = 'Quote',
+                object_id = quote.id,
+                action = 'delete',
+                user = request.user
+            )
+        except ObjectDoesNotExist:
+            quote = None
+    return redirect('shop:quotes')
